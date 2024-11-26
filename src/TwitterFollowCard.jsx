b4eaefe03f0f06d04cf98ff487265d28b4e22f66
Pablo Lobelle <pablolobellelosada@gmail.com>
2024-11-26 12:31:01 +0100

    last commit

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export function TwitterFollowCard({
 	// eslint-disable-next-line react/prop-types
@@ -10,34 +10,34 @@ export function TwitterFollowCard({
 	// eslint-disable-next-line no-unused-vars
 }) {
 	const [isFollowing, setIsFollowing] = useState(false);
-	const text = isFollowing ? "Siguiendo" : "Seguir";
+	const text = isFollowing ? 'Siguiendo' : 'Seguir';
 	const buttonClassName = isFollowing
-		? "tw-followCard-button is-following"
-		: "tw-followCard-button";
+		? 'tw-followCard-button is-following'
+		: 'tw-followCard-button';
 
 	const handleClick = () => {
 		setIsFollowing(!isFollowing);
 	};
 
 	return (
-		<article className="tw-followCard">
-			<header className="tw-followCard-header">
+		<article className='tw-followCard'>
+			<header className='tw-followCard-header'>
 				<img
-					className="tw-followCard-avatar"
+					className='tw-followCard-avatar'
 					src={`https://unavatar.io/${userName}`}
-					alt="avatar"
+					alt='avatar'
 				/>
-				<div className="tw-followCard-info">
+				<div className='tw-followCard-info'>
 					<strong>{children}</strong>
-					<span className="tw-followCard-infoUserName">
+					<span className='tw-followCard-infoUserName'>
 						{formatUserName(userName)}
 					</span>
 				</div>
 			</header>
 			<aside>
 				<button className={buttonClassName} onClick={handleClick}>
-					<span className="tw-followCard-text">{text}</span>
-					<span className="tw-followCard-stopFollow">Dejar de seguir</span>
+					<span className='tw-followCard-text'>{text}</span>
+					<span className='tw-followCard-stopFollow'>Dejar de seguir</span>
 				</button>
 			</aside>
 		</article>
